4e490ca1465b51d4db22716fe81f958cfb91579f
Werner Van Geit <werner.vangeit@epfl.ch>
2015-05-20 14:29:03 +0200

    Added selIBEA to deap.tools Added eaAlphaMuPlusLambda to deap.algorithms Added ibea algorithm example

--- deap/algorithms.py
+++ deap/algorithms.py
@@ -70,7 +70,8 @@ def varAnd(population, toolbox, cxpb, mutpb):
     # Apply crossover and mutation on the offspring
     for i in range(1, len(offspring), 2):
         if random.random() < cxpb:
-            offspring[i - 1], offspring[i] = toolbox.mate(offspring[i - 1], offspring[i])
+            offspring[
+                i - 1], offspring[i] = toolbox.mate(offspring[i - 1], offspring[i])
             del offspring[i - 1].fitness.values, offspring[i].fitness.values
 
     for i in range(len(offspring)):
@@ -223,8 +224,9 @@ def varOr(population, toolbox, lambda_, cxpb, mutpb):
     shall be in :math:`[0, 1]`, the reproduction probability is
     1 - *cxpb* - *mutpb*.
     """
-    assert (cxpb + mutpb) <= 1.0, ("The sum of the crossover and mutation "
-                                   "probabilities must be smaller or equal to 1.0.")
+    assert (
+        cxpb + mutpb) <= 1.0, ("The sum of the crossover and mutation "
+                               "probabilities must be smaller or equal to 1.0.")
 
     offspring = []
     for _ in xrange(lambda_):
@@ -493,6 +495,61 @@ def eaGenerateUpdate(toolbox, ngen, halloffame=None, stats=None,
 
         record = stats.compile(population) if stats is not None else {}
         logbook.record(gen=gen, nevals=len(population), **record)
+        if verbose:
+            print logbook.stream
+
+    return population, logbook
+
+
+def eaAlphaMuPlusLambda(population, toolbox, mu, _,
+                        cxpb, mutpb, ngen, stats=None, halloffame=None,
+                        verbose=__debug__):
+    """This is the :math:`(~\alpha,\mu~,~\lambda)` evolutionary algorithm."""
+    logbook = tools.Logbook()
+    logbook.header = ['gen', 'nevals'] + (stats.fields if stats else [])
+
+    # Evaluate the individuals with an invalid fitness
+    invalid_ind = [ind for ind in population if not ind.fitness.valid]
+    fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
+    for ind, fit in zip(invalid_ind, fitnesses):
+        ind.fitness.values = fit
+
+    if halloffame is not None:
+        halloffame.update(population)
+
+    record = stats.compile(population) if stats is not None else {}
+    logbook.record(gen=0, nevals=len(invalid_ind), **record)
+    if verbose:
+        print logbook.stream
+
+    parents = population[:]
+
+    # Begin the generational process
+    for gen in range(1, ngen + 1):
+        # Vary the parents
+        if hasattr(toolbox, 'variate'):
+            offspring = toolbox.variate(parents, toolbox, cxpb, mutpb)
+        else:
+            offspring = varAnd(parents, toolbox, cxpb, mutpb)
+
+        population[:] = parents + offspring
+
+        # Evaluate the individuals with an invalid fitness
+        invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+        fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
+        for ind, fit in zip(invalid_ind, fitnesses):
+            ind.fitness.values = fit
+
+        # Update the hall of fame with the generated individuals
+        if halloffame is not None:
+            halloffame.update(offspring)
+
+        # Select the next generation parents
+        parents[:] = toolbox.select(population, mu)
+
+        # Update the statistics with the new population
+        record = stats.compile(population) if stats is not None else {}
+        logbook.record(gen=gen, nevals=len(invalid_ind), **record)
         if verbose:
             print logbook.stream
 
